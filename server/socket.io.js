59d21b4fc4517256fec262d645fca5547711e199
gabrielstuff <gabriel@soixantecircuits.fr>
2015-02-11 18:14:10 +0100

    clean up dead socket state before creating socketio server

--- server/socket.io.js
+++ server/socket.io.js
@@ -1,5 +1,5 @@
 var Fiber = Meteor.npmRequire('fibers'),
-  io = Meteor.npmRequire('socket.io')(Meteor.settings.service.port),
+  io,
   fs = Meteor.npmRequire('fs'),
   request = Meteor.npmRequire('request'),
   mkdirp = Meteor.npmRequire('mkdirp'),
@@ -11,7 +11,58 @@ var Fiber = Meteor.npmRequire('fibers'),
 base = fs.realpathSync('.');
 //if address already in use, it throw an error.
 
-
+var initSocket = function() {
+  Socket.update({}, {
+    $set: {
+      connected: false
+    }
+  }, {
+    multi: true
+  });
+  io = Meteor.npmRequire('socket.io')(Meteor.settings.service.port);
+  io.on('connection', function(socket) {
+    console.log('socket.io: Connected !');
+    saveOrUpdateSocketState(socket, true);
+    socket.on('image-saved', function(data) {
+        Fiber(function() {
+          if (saveCount === 0) {
+            album = Date.now();
+            request.get({
+              url: data.src,
+              encoding: null
+            }, Meteor.bindEnvironment(function(e, r, buffer) {
+              if (e) {
+                console.log('err on request', e)
+              } else {
+                saveFile(data, r, buffer);
+              }
+            }));
+          } else if (saveCount < maxSave) {
+            request.get({
+              url: data.src,
+              encoding: null
+            }, Meteor.bindEnvironment(function(e, r, buffer) {
+              if (e) {
+                console.log('err on request', e)
+              } else {
+                saveFile(data, r, buffer);
+              }
+            }));
+          } else {
+            saveCount = 0;
+          }
+          Meteor.clearTimeout(timeoutHandler);
+          timeoutHandler = Meteor.setTimeout(function() {
+            saveCount = 0;
+          }, timeoutDuration);
+          console.log('new file:', data);
+        }).run();
+      })
+      .on('disconnect', function() {
+        saveOrUpdateSocketState(socket, false);
+      });
+  });
+}
 
 var getNumber = function(name) {
   var nb = (name.replace(Meteor.settings.machine.name, '').replace(Meteor.settings.machine.extension, ''));
@@ -20,6 +71,9 @@ var getNumber = function(name) {
     nb = -1;
   } else {
     nb = Number(nb);
+    if (nb === NaN) {
+      nb = -1
+    }
   }
   return (nb);
 }
@@ -44,6 +98,9 @@ var saveFile = function(data, response, buffer) {
 }
 
 var saveOrUpdateSocketState = function(socket, state) {
+  // the socket.handshake.headers.host is the one from the server not the client :/
+  // need to find a better solution...
+  
   Fiber(function() {
     //console.log(socket.handshake);
     Socket.upsert({
@@ -70,47 +127,4 @@ var updateMaxImageNumber = function() {
   }).run()
 }
 
-io.on('connection', function(socket) {
-
-  console.log('socket.io: Connected !');
-  saveOrUpdateSocketState(socket, true);
-
-  socket.on('image-saved', function(data) {
-      Fiber(function() {
-        if (saveCount === 0) {
-          album = Date.now();
-          request.get({
-            url: data.src,
-            encoding: null
-          }, Meteor.bindEnvironment(function(e, r, buffer) {
-            if (e) {
-              console.log('err on request', e)
-            } else {
-              saveFile(data, r, buffer);
-            }
-          }));
-        } else if (saveCount < maxSave) {
-          request.get({
-            url: data.src,
-            encoding: null
-          }, Meteor.bindEnvironment(function(e, r, buffer) {
-            if (e) {
-              console.log('err on request', e)
-            } else {
-              saveFile(data, r, buffer);
-            }
-          }));
-        } else {
-          saveCount = 0;
-        }
-        Meteor.clearTimeout(timeoutHandler);
-        timeoutHandler = Meteor.setTimeout(function() {
-          saveCount = 0;
-        }, timeoutDuration);
-        console.log('new file:', data);
-      }).run();
-    })
-    .on('disconnect', function() {
-      saveOrUpdateSocketState(socket, false);
-    });
-});
+initSocket();
